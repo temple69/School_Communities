55b7c6590a8401f600f2489a21670020fb3c0bb3
temple69 <utemple169@gmail.com>
2023-05-26 18:46:19 +0100

    user defined typescript type for the form values

---
+++ types/Communities.ts
@@ -0,0 +1,7 @@
+type communityType = {
+    community_name: string;
+    community_description: string;
+    community_category: string;
+    university: string;
+  };
+  export default communityType
