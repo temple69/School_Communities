dcfbebf56eb6ab1db9df4dd3393fc9724cafd6db
Nnaji Uchechukwu Temple <utemple169@gmail.com>
2023-06-21 13:52:01 +0100

    Update Layout.jsx

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,12 +1,1 @@
-import React from 'react'
-import NavBar from './Navbar'
-const Layout = ({children}) => {
-  return (
-    <main>
-        <NavBar/>
-        {children}
-    </main>
-  )
-}
 
-export default Layout
