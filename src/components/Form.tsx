57a0e52d84b17d61e39d0f1d4357eca01a50750a
temple69 <utemple169@gmail.com>
2023-07-05 17:01:52 +0100

    added extra input fields to the from and made some changes to input

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -8,8 +8,11 @@ const Form = () => {
   const [formValues, setFormValues] = useState<Communities>({
     community_name: "",
     community_description: "",
+    community_abbreviation: "",
     community_category: "",
     university: "",
+    community_logo: "",
+    university_logo: "",
   });
   const router = useRouter();
   const [isLoading, setIsLoading] = useState(false);
@@ -35,9 +38,11 @@ const Form = () => {
     community_description,
     community_name,
     community_category,
+    community_abbreviation,
     university,
+    community_logo,
+    university_logo,
   } = formValues;
-  
   const formHandler = async (event: FormEvent) => {
     event.preventDefault();
     if (
@@ -50,17 +55,10 @@ const Form = () => {
     ) {
       alert("Please complete the input values");
       return;
-      
-
     }
-    const containsUpperCase = /[A-Z]/
-    /* if (containsUpperCase.test(university) !) {
-      university.toLowerCase()
-    
-      
-    } */
-    const rem =university.length
-    console.log(university[0].toLowerCase() + university.slice(1,rem) )
+    const containsUpperCase = /[A-Z]/;
+  
+    console.log(university.replace(/\s/g, ""));
     setIsLoading(true);
     try {
       const response = fetch("api/community/add_community", {
@@ -74,9 +72,8 @@ const Form = () => {
       console.log(data);
       if (data.Success) {
         toast.success(data.message, {
-          position: toast.POSITION.TOP_RIGHT
-      });
-
+          position: toast.POSITION.TOP_RIGHT,
+        });
       }
     } catch (error) {
       console.log(error);
@@ -87,29 +84,72 @@ const Form = () => {
   return (
     <section className="px-4 flex justify-center">
       {isLoading ? <Modal /> : ""}
-      <form onSubmit={formHandler} className="w-[60%]">
-        <label className="block text-[20px] my-2">
-          Name of Community:
-          <input
-            onChange={inputHandler("community_name")}
-            required
-            value={community_name}
-            type="text"
-            placeholder="Google Developer Student Group"
-            className="w-full border pl-4 h-10 rounded-lg"
-            name="community_name"
-          />
-        </label>
+      <form onSubmit={formHandler} className="w-[60%] text-black">
+        {/* Fieldset grouping for the community name and abbreviation */}
+        <fieldset className="grid grid-cols-2 gap-4">
+          <label className="block text-[20px] my-2">
+            Name of Community:
+            <input
+              onChange={inputHandler("community_name")}
+              required
+              value={community_name}
+              type="text"
+              placeholder="Google Developer Student Group"
+              className="w-full border pl-4 h-10 rounded-lg"
+              name="community_name"
+            />
+          </label>
+          <label className="block text-[20px] my-2">
+            Community Abbreviation:
+            <input
+              onChange={inputHandler("community_abbreviation")}
+              required
+              value={community_abbreviation}
+              type="text"
+              placeholder="GDSC"
+              className="w-full border pl-4 h-10 rounded-lg"
+              name="community_abbreviation"
+            />
+          </label>
+        </fieldset>
+        {/* Fieldset grouping for the community image  and university image */}
+        <fieldset className="grid grid-cols-2 gap-4">
+          <label className="block text-[20px] my-2">
+            Community Logo:
+            <input
+              onChange={inputHandler("community_logo")}
+      
+              value={community_logo}
+              type="text"
+              placeholder=""
+              className="w-full border pl-4 h-10 rounded-lg"
+              name="community_logo"
+            />
+          </label>
+          <label className="block text-[20px] my-2">
+            University Logo:
+            <input
+              onChange={inputHandler("university_logo")}
+            
+              value={university_logo}
+              type="text"
+              placeholder=""
+              className="w-full border pl-4 h-10 rounded-lg"
+              name="university_logo"
+            />
+          </label>
+        </fieldset>
+
         <label htmlFor="" className="block text-[20px] my-2">
-          Community Category:
-          <select
-            onChange={inputHandler("community_category")}
-            required
+          Select / Create Community Category:
+          <input
+            list="Communities"
             value={community_category}
-            name="community_category"
+            onChange={inputHandler("community_category")}
             className="border block w-full h-10 roundes-sm pl-4 rounded-lg"
-          >
-            <option value=""></option>
+          />
+          <datalist id="Communities">
+            hello
             <option value="Tech">
               Tech(Programming,Coding,DataScience, etc)
             </option>
@@ -118,7 +158,25 @@ const Form = () => {
             </option>
             <option value="Social">Social(Entertainment,school Tv)</option>
             <option value="Finance">Finance</option>
-          </select>
+            <option value="CyberSecuity" >CyberSecurity(Ethical Hacker,Cybersec analyst etc)</option>
+          </datalist>
+         {/*  <select
+            onChange={inputHandler("community_category")}
+            required
+            value={community_category}
+            name="community_category"
+            className="border block w-full h-10 roundes-sm pl-4 rounded-lg"
+          >
+            <option value=""></option>
+            <option value="Tech">
+              Technology(Programming,Coding,DataScience, etc)
+            </option>
+            <option value="Blockchain">
+              Blockchain(web3,Defi,CryptoCurrency)
+            </option>
+            <option value="Social">Social(Entertainment,school Tv)</option>
+            <option value="Finance">Finance</option>
+          </select> */}
         </label>
 
         <label htmlFor="" className="text-[20px] my-2">
@@ -143,7 +201,7 @@ const Form = () => {
             id=""
             cols={30}
             rows={5}
-            className="border block resize-none w-full pl-4 rounded-lg"
+            className="border block resize-none w-full pl-4 rounded-lg text-black"
           ></textarea>
         </label>
         <label className="w-full text-center block">
