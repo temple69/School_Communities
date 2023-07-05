ad108fe1e410b0d54c01338020a67975e871bd0f
temple69 <utemple169@gmail.com>
2023-07-05 17:30:46 +0100

    updated types and included new fields

--- types/Communities.ts
+++ types/Communities.ts
@@ -3,5 +3,8 @@ type communityType = {
     community_description: string;
     community_category: string;
     university: string;
+    community_abbreviation: string;
+    community_logo: string;
+    university_logo: string;
   };
   export default communityType
